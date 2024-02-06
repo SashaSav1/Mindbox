3c914d8134bc33afaa65111b8047322930335344
sashakrlov@gmail.com <sashakrlov@gmail.com>
2024-02-06 18:30:55 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,69 @@
+﻿namespace ClassLibrary1
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IShape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            if (radius < 0)
+                throw new ArgumentException("Радиус не может быть отрицательным");
+
+            double S = Math.PI * radius * radius;
+
+            return S;
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        private double side1;
+        private double side2;
+        private double side3;
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+
+        public double CalculateArea()
+        {
+            if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+                throw new ArgumentException("Длины сторон должны быть положительными");
+
+            if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+                throw new ArgumentException("Треугольник с такими сторонами не может существовать");
+
+            double p = (side1 + side2 + side3) / 2;
+            double S = Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
+
+            return S;
+        }
+        public bool IsRectangularTriangle()
+        {
+            double[] sides = { side1, side2, side3 };
+            Array.Sort(sides);
+
+            if(Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Mindbox/Program.cs
@@ -0,0 +1,34 @@
+﻿using ClassLibrary1;
+
+// Круг
+Circle Circle = new Circle(56);
+try
+{
+    Console.WriteLine("Площадь круга: " + Circle.CalculateArea());
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("Произошла ошибка: " + ex.Message);
+}
+
+// Треугольник
+Triangle Triangle = new Triangle(10, 4, 5);
+try
+{
+    Console.WriteLine("Площадь треугольника: " + Triangle.CalculateArea());
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("Произошла ошибка: " + ex.Message);
+}
+
+if (Triangle.IsRectangularTriangle())
+{
+    Console.WriteLine("Треугольник прямоугольный");
+}
+else
+{
+    Console.WriteLine("Треугольник не прямоугольный");
+}
+
+Console.ReadLine();
